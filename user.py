d77115905936e494d801f03599c835d94f9cc4a5
Robert Godlewski <robtechg5@gmail.com>
2022-01-10 17:38:26 -0800

    Edited most methods to chain when called multiple times.

--- user.py
+++ user.py
@@ -5,18 +5,19 @@ class User:
 
     def make_deposit(self, amount): 
         self.balance += amount
-        return self.balance
+        return self
 
     def make_withdrawl(self, amount):
         self.balance -= amount
-        return self.balance
+        return self
 
     def display_user_balance(self):
         return "User: " + str(self.user_name) + ", Balance: " + str(self.balance)
 
-    def tansfer_money(self, to_user, amount):
-        self.balance -= amount
-        to_user.balance += amount
+    def transfer_money(self, to_user, amount):
+        self.make_withdrawl(amount)
+        to_user.make_deposit(amount)
+        return self
 
 
 # Testing
@@ -24,6 +25,8 @@ rob_bal = User('rob', 500)
 nate_bal = User('nate', 50)
 tristan_bal = User('tristan', 5)
 
+# Testing for Users
+'''
 rob_bal.make_deposit(10000)
 rob_bal.make_deposit(5000)
 rob_bal.make_withdrawl(1000)
@@ -44,4 +47,13 @@ print(tristan_bal.display_user_balance())
 
 rob_bal.tansfer_money(tristan_bal, 100)
 print(rob_bal.display_user_balance())
+print(tristan_bal.display_user_balance())
+'''
+
+# Testing for Chaining Methods
+print(rob_bal.make_deposit(10000).make_deposit(5000).make_withdrawl(1000).make_deposit(500).display_user_balance())
+print(nate_bal.make_deposit(500000).make_withdrawl(2000).make_deposit(5000).make_withdrawl(700).display_user_balance())
+print(tristan_bal.make_deposit(5000).make_withdrawl(500).make_withdrawl(5).make_withdrawl(100).display_user_balance())
+
+print(rob_bal.transfer_money(tristan_bal, 100).display_user_balance())
 print(tristan_bal.display_user_balance())
